e13385c2bd85941298eed9d2fc538b8d51242e9d
Wonderland6627 <994015020@qq.com>
2025-05-08 20:06:51 +0800

    添加批量替换字体脚本

--- UnityProject/Assets/GameScripts/HotFix/GameLogic/UITools/FontGetter.cs
+++ UnityProject/Assets/GameScripts/HotFix/GameLogic/UITools/FontGetter.cs
@@ -3,35 +3,50 @@ using System.Collections;
 using System.Collections.Generic;
 using TEngine;
 using UnityEngine;
+using UnityEngine.UI;
 using WeChatWASM;
 
 namespace GameLogic
 {
-    public class FontGetter
+    public class FontGetter : MonoBehaviour
     {
-        private static Font WXFont;
+        public Text[] allTexts;
 
-        public static void LoadFont(string fallbackUrl, Action<Font> onComplete)
+        public string logTitle;
+        public static Font wxFont;
+
+        void Start()
         {
-            if (WXFont != null)
+            logTitle = gameObject.name;
+            allTexts = transform.GetComponentsInChildren<Text>(true);
+            if (wxFont != null)
             {
-                Log.Info($"[FontGetter] WXFont exists, font: {WXFont.name}");
-                onComplete.Invoke(WXFont);
+                ApplyFont();
                 return;
             }
-            WX.GetWXFont(fallbackUrl, (font) => 
+
+            var fontName = "cartoon_font_1.ttf";
+            var fallbackFontUrl = $"https://a.unity.cn/client_api/v1/buckets/cde09f24-d39c-4845-a3e3-17344f4f2894/content/MiniGame/Fonts/{fontName}";
+            WX.GetWXFont(fallbackFontUrl, (font) =>
             {
                 if (font == null)
                 {
-                    Log.Error("[FontGetter] GetWXFont fail");
-                    onComplete.Invoke(null);
+                    Log.Error($"[FontGetter - {logTitle}] GetWXFont failed");
                     return;
                 }
-
-                WXFont = font;
-                onComplete.Invoke(WXFont);
-                Log.Info($"[FontGetter] GetWXFont success, font: {font.name}");
+                wxFont = font;
+                ApplyFont();
             });
+        }
+
+        void ApplyFont()
+        {
+            for (int i = 0; i < allTexts.Length; i++)
+            {
+                if (allTexts[i].font == wxFont) continue;
+                allTexts[i].font = wxFont;
+            }
+            Log.Info($"[FontGetter - {logTitle}] GetWXFont success, replace text font to {wxFont.name}");
         }
     }
 }
